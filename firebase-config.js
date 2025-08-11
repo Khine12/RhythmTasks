83618dad84f7ee5b9bca854eb4a3a682738b3815
Khine Zar Hein <khinezarhein1@gmail.com>
2025-08-10 23:43:06 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// ✅ Correct Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD59G2HyQvJsp2aVP-qXT9B7csPRuT5wCY",
   authDomain: "rhythmtasks-b58b7.firebaseapp.com",
@@ -13,7 +11,6 @@ const firebaseConfig = {
   appId: "1:382556143960:web:d66e16a4e8097cd8f6652d"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
